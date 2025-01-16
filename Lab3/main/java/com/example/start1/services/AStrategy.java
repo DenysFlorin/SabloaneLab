7d492f49c715e6b7ad096fb23491469a219d2eb8
DenysFlorin <118625243+DenysFlorin@users.noreply.github.com>
2025-01-16 23:12:25 +0200

    Delete Lab3 directory

--- Lab3/main/java/com/example/start1/services/AStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.example.start1.services;
-
-public enum AStrategy {
-    LEFT,
-    RIGHT,
-    CENTER,
-    JUSTIFY
-}
--- Lab3/main/java/com/example/start1/services/Strategy.java
+++
@@ -1,7 +0,0 @@
-package com.example.start1.services;
-
-import com.example.start1.models.Paragraph;
-
-public interface Strategy {
-    public void render(Paragraph paragraph);
-}
